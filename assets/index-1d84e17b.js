32a4afff2c3e77bb57df60bd3f90361a0c2ef2f9
marioliu2001 <62660112+marioliu2001@users.noreply.github.com>
2023-11-30 06:50:01 +0000

    Deploying to gh-pages from @ marioliu2001/vue3-template-admin@9eb4c63282f6277a4ef942ed6479ebc4ae0b5b55 🚀

---
+++ assets/index-1d84e17b.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-0db056dc.js";const s={},t={class:"reports"};function r(n,_){return c(),o("div",t," Reports ")}const d=e(s,[["render",r]]);export{d as default};
